420fbee152ace5332e6e5b535b32df0be3919a30
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-09-17 17:24:43 +0200

    Sync

--- resources/views/profile/settings.blade.php
+++ resources/views/profile/settings.blade.php
@@ -4,6 +4,9 @@
     {{ trans('acacha-profile_lang::message.settings') }}
 @endsection
 
+@section('contentheader_title')
+    {{ trans('acacha-profile_lang::message.settings') }}
+@endsection
 
 @section('main-content')
     <div class="row">
@@ -12,9 +15,9 @@
             <!-- Profile Image -->
             <div class="box box-primary">
                 <div class="box-body box-profile">
-                    <img class=z"profile-user-img img-responsive img-circle" src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
+                    <img class="profile-user-img img-responsive img-circle" src="{{ Gravatar::get($user->email) }}" alt="User profile picture">
 
-                    <h3 class="profile-username text-center">Pepita Palotes AAA</h3>
+                    <h3 class="profile-username text-center">{{ $user->name }}</h3>
 
                     <p class="text-muted text-center">Software Engineer</p>
 
